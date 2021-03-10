ac197baa98566df401876dbcca86357be368b42b
TJGit11 <74613952+TJGit11@users.noreply.github.com>
2021-03-10 14:02:44 -0600

    weblayertesthw

---
+++ src/main/java/com/tts/WebLayerTest/WebLayerTestApplication.java
@@ -0,0 +1,13 @@
+package com.tts.WebLayerTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebLayerTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebLayerTestApplication.class, args);
+	}
+
+}
